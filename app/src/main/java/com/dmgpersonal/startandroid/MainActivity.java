e50329609d7cf538afefa361f1c987676ea78ec9
Dmitriy Gridchin <tigris.tstu@gmail.com>
2021-07-02 20:53:56 +0300

    демонстрация работы с интентами, вызов приложения. вызывающее приложение.

--- app/src/main/java/com/dmgpersonal/startandroid/MainActivity.java
+++ app/src/main/java/com/dmgpersonal/startandroid/MainActivity.java
@@ -2,13 +2,28 @@ package com.dmgpersonal.startandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final static String TEXT = "PARAM";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText editText = findViewById(R.id.editText);
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(v -> {
+            Uri uri = Uri.parse("example://intent");
+            Intent runEchoIntent = new Intent(Intent.ACTION_VIEW, uri);
+            runEchoIntent.putExtra(TEXT, editText.getText().toString());
+            startActivity(runEchoIntent);
+        });
     }
 }
