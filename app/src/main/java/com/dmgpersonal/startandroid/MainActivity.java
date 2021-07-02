8e6820ed0686b967c503eb07256c1cb9057c62b5
Dmitriy Gridchin <tigris.tstu@gmail.com>
2021-07-02 20:31:54 +0300

    Демонстрация обработки иртента и вызова другого приложения - вызываемое приложение

--- app/src/main/java/com/dmgpersonal/startandroid/MainActivity.java
+++ app/src/main/java/com/dmgpersonal/startandroid/MainActivity.java
@@ -2,13 +2,26 @@ package com.dmgpersonal.startandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final static String TEXT = "PARAM";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = getIntent();
+        Bundle bundle = intent.getExtras();
+        if(bundle == null) {
+            return;
+        }
+        String text = bundle.getString(TEXT);
+        TextView textView = findViewById(R.id.textEcho);
+        textView.setText(text);
     }
 }
